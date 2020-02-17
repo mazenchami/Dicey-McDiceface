8f34f8aa8cdf237825e689db75035762f77fce89
Mazen Chami <mazenchami@Mazens-MacBook-Pro.local>
2020-02-17 13:22:15 -0500

    building storybook

---
+++ storybook-static/precache-manifest.dcf88c3019576c19e78157e8306a5dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e455e1bfabfa8e7a0688117308aa1bde",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a6608b2b795989e15144.bundle.js"
+  },
+  {
+    "url": "runtime~main.a6608b2b795989e15144.bundle.js"
+  },
+  {
+    "revision": "c2dd134c06326957d607",
+    "url": "static/css/main.5d53bd4e.chunk.css"
+  },
+  {
+    "revision": "63d9823832b46769b57c",
+    "url": "static/css/vendors~main.47e06e2e.chunk.css"
+  },
+  {
+    "url": "vendors~main.a6608b2b795989e15144.bundle.js"
+  },
+  {
+    "url": "vendors~main.a6608b2b795989e15144.bundle.js.LICENSE.txt"
+  }
+]);
